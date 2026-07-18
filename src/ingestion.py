74aa60d21a696432e245fd9669696a8a59e1a15a
AD <eddiegotaipro@gmail.com>
2026-07-18 17:06:41 +1000

    Get the link to download files for both abs and rba

--- src/ingestion.py
+++ src/ingestion.py
@@ -4,8 +4,11 @@
 
 
 def download_abs_dwelling_values():
+    # The first link https://www.abs.gov.au/statistics/economy/price-indexes-and-inflation/total-value-dwellings/mar-quarter-2026/643201.xlsx
+    # The second link https://www.abs.gov.au/statistics/economy/price-indexes-and-inflation/total-value-dwellings/mar-quarter-2026/643202.xlsx
     print("successfully download ABS dwelling values")
 
 
 def download_rba_cash_rate():
+    # https://www.rba.gov.au/statistics/tables/xls/f01d.xlsx
     print("successfully download RBA cash rate")
