b407a98d61a0249a2c35889eb5eb6b4e039955a7
AD <eddiegotaipro@gmail.com>
2026-07-18 16:40:49 +1000

    Initialise the ingestion functions for both abs and rba

---
+++ src/ingestion.py
@@ -0,0 +1,11 @@
+# I should write two functions here
+#
+# Their job is to fetch the source file, and write them into data/raw
+
+
+def download_abs_dwelling_values():
+    print("successfully download ABS dwelling values")
+
+
+def download_rba_cash_rate():
+    print("successfully download RBA cash rate")
